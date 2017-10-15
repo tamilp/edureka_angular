9e5396772ca7d333060a1d1f98db8afaa84298a4
tamilp <limatt20@gmail.com>
2017-10-14 21:54:50 -0400

    angular project

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'pm-app',
+	template: `<div>
+		<h1>This is ang2</h1>
+		<author-component></author-component>
+	</div>`
+})
+
+export class AppComponent{
+
+}
---
+++ app/author.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'author-component',
+    template:`
+    <h1>Author</h1>
+    `
+})
+
+export class AuthorComponent{
+    
+}
